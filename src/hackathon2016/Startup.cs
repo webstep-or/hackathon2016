e819dbe7b9a9deed7a5d0537cb20cf996eee557e
webstep-or <orjan.rafdal@webstep.no>
2016-04-19 19:35:59 +0200

    start

--- src/hackathon2016/Startup.cs
+++ src/hackathon2016/Startup.cs
@@ -22,10 +22,8 @@ namespace hackathon2016
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
